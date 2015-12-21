5ca6d0144faf7ed219d3676bc6d680e089874048
Lorian Coltof <loriancoltof@gmail.com>
2015-12-21 21:28:02 +0100

    Fixed exit menu

--- i3/exit_terminal.sh
+++ i3/exit_terminal.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gnome-terminal -t "Quit i3" --geometry=40x10 --hide-menubar --window-with-profile=NoScrollbar -e "bash -c \"~/.i3/exit.py\"" 
+_SCRIPTDIR=$(cd $(dirname $0);echo $PWD)
+gnome-terminal -t "Quit i3" --geometry=40x10 --hide-menubar --window-with-profile=NoScrollbar -e \
+	"bash -c \"$_SCRIPTDIR/exit.py\"" 

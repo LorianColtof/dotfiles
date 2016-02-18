7a029a16e32a4d5b54cc58c66b3aca6a4246ab88
Lorian Coltof <loriancoltof@gmail.com>
2016-02-18 18:36:34 +0100

    Battery to relative percentage

--- i3/i3statusconf.py
+++ i3/i3statusconf.py
@@ -6,23 +6,16 @@ status = Status(standalone=True)
 # Displays clock like this:
 # Tue 30 Jul 11:59:46 PM Week 31
 #                          ^-- calendar week
-status.register("clock",
-    format="%a %-d %b %Y %T Week %V",
-)
+status.register("clock", format="%a %-d %b %Y %T Week %V")
 
 # Show CPU usage bar
-status.register("cpu_usage_bar",
-	format="CPU:{usage_bar}",)
+status.register("cpu_usage_bar", format="CPU:{usage_bar}")
 
 # Shows your CPU temperature, if you have a Intel CPU
-status.register("temp",
-    format="{temp:.0f}°C",)
+status.register("temp", format="{temp:.0f}°C")
 
 if os.path.isdir("/sys/class/backlight/intel_backlight"):
-	status.register("backlight",
-		format="Backlight: {percentage}%",
-		backlight="intel_backlight"
-		)
+    status.register("backlight", format="Backlight: {percentage}%", backlight="intel_backlight")
 
 
 # The battery monitor has many formatting options, see README for details
@@ -37,17 +30,17 @@ if os.path.isdir("/sys/class/backlight/intel_backlight"):
 # If you don't have a desktop notification demon yet, take a look at dunst:
 #   http://www.knopwob.org/dunst/
 if os.path.isdir("/sys/class/power_supply/BAT0"):
-	status.register("battery",
-		format="{status} |{bar_design}| {percentage_design:.0f}% {remaining:%E%hh:%Mm} {consumption:.2f}W",
-		not_present_text="(no battery)",
-		alert=False,
-		alert_percentage=4,
-		color='#4b6dff',
-		status={
-			"DIS": "↓",
-			"CHR": "↑",
-			"FULL": "=",
-		},)
+    status.register("battery",
+                    format="{status} |{bar}| {percentage:.2f}% {remaining:%E%hh:%Mm} {consumption:.2f}W",
+                    not_present_text="(no battery)",
+                    alert=False,
+                    alert_percentage=4,
+                    color='#4b6dff',
+                    status={
+                        "DIS": "↓",
+                        "CHR": "↑",
+                        "FULL": "=",
+                    })
 
 # Shows the address and up/down state of eth0. If it is up the address is shown in
 # green (the default value of color_up) and the CIDR-address is shown
@@ -57,30 +50,29 @@ if os.path.isdir("/sys/class/power_supply/BAT0"):
 #
 # Note: the network module requires PyPI package netifaces
 status.register("network",
-    interface="eth0",
-	format_down="{interface}",
-    format_up="{interface}: {v4cidr}",)
+                interface="eth0",
+                format_down="{interface}",
+                format_up="{interface}: {v4cidr}",)
 
 # Note: requires both netifaces and basiciw (for essid and quality)
 status.register("network",
-    interface="wlan0",
-	format_down="{interface}",
-    format_up="{interface}: {essid} {quality:03.0f}% ({v4cidr})",)
+                interface="wlan0",
+                format_down="{interface}",
+                format_up="{interface}: {essid} {quality:03.0f}% ({v4cidr})")
 
 # Shows disk usage of /
 # Format:
 # 42/128G [86G]
 status.register("disk",
-    path="/",
-    format="{used}/{total}GB [{avail}GB]",)
+                path="/",
+                format="{used}/{total}GB [{avail}GB]")
 
 # Shows pulseaudio default sink volume
 #
 # Note: requires libpulseaudio from PyPI
 status.register("pulseaudio",
-    format="{muted} ♪ {volume}%",
-	muted=" (muted)"
-	)
+                format="{muted} ♪ {volume}%",
+                muted=" (muted)")
 
 
 status.run()

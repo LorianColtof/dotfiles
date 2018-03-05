a505934a798901cd57c713a3c163daa9f558eb2c
Lorian Coltof <loriancoltof@gmail.com>
2018-03-06 00:01:38 +0100

    Remove crap

--- i3/exit_terminal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-_SCRIPTDIR=$(cd $(dirname $0);echo $PWD)
-termite -t "Quit i3" --geometry 300x150 -e "zsh -c 'source ~/.zshrc; $_SCRIPTDIR/exit.py'"
-
-
--- i3/start_arandr.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import i3
-
-if __name__=="__main__":
-	workspace = [i for i in range(1,10) if not i in [w['num'] for w in i3.get_workspaces()]][0]
-	i3.command('workspace', str(workspace))
-	i3.command('exec', 'arandr')

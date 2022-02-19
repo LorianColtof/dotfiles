5625446d900214645f25c9c129f2233f574cf841
Lorian Coltof <loriancoltof@gmail.com>
2022-02-19 14:44:29 +0100

    Update setup_background.sh script

--- i3/.config/i3/setup_background.sh
+++ i3/.config/i3/setup_background.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
 BACKGROUNDS=(~/wallpapers/*)
+CURRENT_BG_PATH=~/.current_background
 
-index=$(($RANDOM % ${#BACKGROUNDS[@]}))
+case $1 in
+    --random)
+        index=$(($RANDOM % ${#BACKGROUNDS[@]}))
+        ;;
+    --current)
+        if [[ ! -e $CURRENT_BG_PATH ]]; then
+            echo "Missing $CURRENT_BG_PATH"
+            exit 1
+        fi
+
+        index=$(cat $CURRENT_BG_PATH)
+        ;;
+    *)
+        echo "Usage: $0 [--random|--current]"
+        exit 1
+        ;;
+esac
+
 BACKGROUND=${BACKGROUNDS[$index]}
 feh --bg-fill $BACKGROUND
+echo $index > $CURRENT_BG_PATH
